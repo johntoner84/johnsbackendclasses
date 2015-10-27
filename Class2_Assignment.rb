7d6b59679abad50c15395b95bc5efcbc246d43ff
John Toner <johntoner84@gmail.com>
2015-10-27 17:57:52 -0400

    trying again

---
+++ Class2_Assignment.rb
@@ -0,0 +1,41 @@
+# 1.Create a program to analyze a large block of text and report back on the frequency of each word 
+
+# in the text. Start by sorting the text into a hash where each word is a key with the word's 
+# value being the amount of times it has been used in the block:
+#     {the: 10, john: 1, of: 15}
+
+# Once you've created this hash, return the word that has been used the most
+# When you're done, encapsulate your script inside of a method that can
+#  analyze any block of text fed to it
+
+
+	
+# Approach:
+# 1.Create a program to analyze a large block of text and report back on the frequency of each word 
+
+# Step 1, create my method to look within a sentence and find a series of words
+
+def frequencyofwords  (sentence, word)
+
+end
+
+
+# Step 2 create an array with my sentence and words
+
+sentence = "this is a sentence that is a beautiful sentence and is great"
+word = ["this","is","sentence","that"]
+
+
+
+
+sentence.count (word)
+
+# in the text. Start by sorting the text into a hash where each word is a key with the word's 
+# value being the amount of times it has been used in the block:
+#     {the: 10, john: 1, of: 15}
+
+# Once you've created this hash, return the word that has been used the most
+# When you're done, encapsulate your script inside of a method that can
+#  analyze any block of text fed to it
+
+	
